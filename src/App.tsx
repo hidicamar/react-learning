51b6cc89f5bdaf4485797ecb1d6cb1ddd6117b15
hidicamar <hidicamar@hotmail.com>
2024-06-04 23:14:12 +0200

    Practice

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,48 @@ import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-    const cities = ["New york", "Amsterdam", "Berlin"];
+    //const [game, setGame] = useState({
+    //    id: 1,
+    //    player: {
+    //        name: "John",
+    //    },
+    //});
 
-    const [alertVisibility, setAlertVisibility] = useState(false);
+    //const [pizza, setPizza] = useState({
+    //    name: "spicy Pepperoni",
+    //    toppings: ["Mushroom"],
+    //});
 
-    const handleListItemSelection = (item: string) => {
-        console.log(item);
+    const [cart, setCart] = useState({
+        discount: 0.1,
+        items: [
+            { id: 1, title: "Product 1", quantity: 1 },
+            { id: 2, title: "Product 2", quantity: 1 },
+        ],
+    });
+
+    const handleClick = () => {
+        // setGame({ ...game, player: { ...game.player, name: "New name" } });
+        // setPizza({ ...pizza, toppings: [...pizza.toppings, "Test"] });
+        setCart({
+            ...cart,
+            items: cart.items.map((item) =>
+                item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+            ),
+        });
     };
 
     return (
         <>
-            {alertVisibility && (
-                <Alert onClose={() => setAlertVisibility(false)}>
-                    test allert
-                </Alert>
-            )}
+            <ul className="divide-y divide-gray-100">
+                {cart.items.map((item, index) => (
+                    <li className="py-3 bg-slate-300" key={item.id}>
+                        {item.title}, {item.quantity}
+                    </li>
+                ))}
+            </ul>
 
-            <Button
-                variant="secondary"
-                onClick={() => setAlertVisibility(true)}
-            >
-                Alert
-            </Button>
-
-            <ListGroup
-                items={cities}
-                heading="My list"
-                onSelectItem={handleListItemSelection}
-            ></ListGroup>
+            <Button onClick={handleClick}>Change name</Button>
         </>
     );
 }
